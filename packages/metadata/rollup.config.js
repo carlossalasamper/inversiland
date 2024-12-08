afa70f10a2ac93551baaa66bc22c3bb8c2dff550
Carlos Sala Samper <carlos15695@hotmail.com>
2024-12-08 17:42:57 +0200

    fix(shared): createRollupConfig has now mjs extension

--- packages/metadata/rollup.config.js
+++ packages/metadata/rollup.config.js
@@ -1,4 +1,4 @@
-import createRollupConfig from "../../scripts/createRollupConfig.js";
+import createRollupConfig from "../../scripts/createRollupConfig.mjs";
 import packageJson from "./package.json" with { type: "json" };
 
 export default createRollupConfig(packageJson);
